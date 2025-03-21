fb4b9660e0b70f2e95cf877f8eb7a24a44ed2d8e
karol203 <karolnowakowski154@gmail.com>
2025-03-21 19:07:57 +0100

    drugi commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    fahr = lower;
+    printf("Celsius: \t Fahrenheit:\n");
+    while(fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%7.1d %19.0d \n",celsius,fahr);
+        fahr = fahr + step;
+    }
+    printf("Done");
+}

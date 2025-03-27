e2bfb7b4eb475703a26da7ac372d49b2f65acb1c
karol203 <karolnowakowski154@gmail.com>
2025-03-27 19:11:47 +0100

    Add files via upload

---
+++ Instrukcje warunkowe i wyboru.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+int main1()
+{
+
+    //zadanie 1.3.1
+    int n;
+    printf("Podaj liczbe n: \n");
+    scanf("%d", &n);
+    if (n<0)
+    {
+        printf("Wartosc bezwzgledna z n to: %d", -n);
+    }
+    else
+    {
+        printf("Wartosc bezwzgledna z n to: %d", n);
+    }
+
+    //zadanie 1.3.2
+    int a,b;
+    printf("podaj libcze a i b: \n");
+    scanf("%d %d", &a, &b);
+    if(a<b)
+    {
+        printf("Liczba b: %d jest wieksza od liczby a: %d", b, a);
+    }
+    else if(a>b)
+    {
+        printf("Liczba a: %d jest wieksza od liczby b: %d", a, b);
+    }
+    else
+    {
+        printf("Liczby a i b sa takie same");
+    }
+
+    //zadanie 1.3.3
+    int a,b,c;
+    printf("Podaj 3 liczby: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>b && a>c)
+    {
+        printf("Najwieksza liczba z 3 podanych to: %d", a);
+    }
+    else if(b>c && b>a)
+    {
+        printf("Najwieksza liczba z 3 podanych to: %d", b);
+    }
+    else if(c>a && c>b)
+    {
+        printf("Najwieksza liczba z 3 podanych to: %d", c);
+    }
+    else if(a == b)
+    {
+        if(a>c)
+        {
+            printf("Sa 2 najwieksze liczby: %d i %d", a, b);
+        }
+        else if(a<c)
+        {
+            printf("Najwieksza liczba to: %d", c);
+        }
+        else
+        {
+            printf("Sa 3 najwieksze liczby: %d %d %d", a, b, c);
+        }
+    }
+    else if(b == c)
+    {
+        if(b>a)
+        {
+            printf("Sa 2 najwieksze liczby: %d i %d", b, c);
+        }
+        else if(b<a)
+        {
+            printf("Najwieksza liczba to: %d", a);
+        }
+    }
+    else
+    {
+        printf("Sa 2 najwieksze liczby: %d i %d", a, c);
+    }
+
+    //zadanie 1.3.4
+    int a,b,c;
+    printf("Podaj 2 liczby calkowite: \n");
+    scanf("%d %d", &a, &b);
+    if(a<0 && b<0)
+    {
+        a = a * -1;
+        b = b * -1;
+        if(a>b)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest a");
+        }
+
+        else if(b>a)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest b");
+        }
+        else
+        {
+            printf("Liczby sa sobie rowne");
+        }
+    }
+    else if(a<0)
+    {
+        a = a * -1;
+        if(a>b)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest a");
+        }
+
+        else if(b>a)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest b");
+        }
+        else
+        {
+            printf("Liczby sa sobie rowne");
+        }
+    }
+    else if(b<0)
+    {
+        b = b * -1;
+         if(a>b)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest a");
+        }
+
+        else if(b>a)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest b");
+        }
+        else
+        {
+            printf("Liczby sa sobie rowne");
+        }
+    }
+    else
+    {
+        if(a>b)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest a");
+        }
+
+        else if(b>a)
+        {
+            printf("Liczba o wiekszej wartosci bezwzgldnej jest b");
+        }
+        else
+        {
+            printf("Liczby sa sobie rowne");
+        }
+    }
+
+    //zadanie 1.3.5
+    unsigned int a,b,c,h;
+    float pole;
+    printf("Podaj dlugosc podstawy trojkata: \n");
+    scanf("%d", &a);
+    printf("Podaj drugi bok trojkata: \n");
+    scanf("%d", &b);
+    printf("Podaj trzeci bok trojkata: \n");
+    scanf("%d", &c);
+    printf("Podaj wysokosc trojkata: \n");
+    scanf("%d", &h);
+    pole = 0.5*a*h;
+    printf("Pole trojkata = %f", pole);
+
+    //zadanie 1.3.6
+    int a,b,c,d,e,f,Wx,Wy,W;
+    printf("Podaj wspolczynniki pierwszego rownania: \n");
+    scanf("%d %d", &a, &b);
+    printf("Podaj wspolczynniki drugiego rownania: \n");
+    scanf("%d %d", &d, &e);
+    W = a*e-b*d;
+    printf("uklad rownan to: %dx + %dy = c \n\t\t %dx + %dy = c", a, b, d, e);
+    return 0;
+    */
+}
---
+++ operacje wejścia wyjścia.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+int main3()
+{
+
+    //zadanie 1.2.1
+    printf("Hello world!\n");
+
+    //zadanie 1.2.2
+    printf("Bardzo \n dlugi \n napis \n");
+
+    //zadanie 1.2.3
+    printf("// \\ \\ $ & %% \n");
+
+    //zadanie 1.2.4
+    int c;
+    printf("Podaj liczbe calkowita: \n");
+    scanf("%d", &c);
+    printf("Podana liczba to : %d \n", c);
+
+    //zadanie 1.2.5
+    float a;
+    printf("Podaj liczbe wymierna: \n");
+    scanf("%f", &a);
+    printf("Podana liczba to: %f", a);
+
+    //zadanie 1.2.6
+    int a,b,c;
+    printf("Podaj 3 liczby ca³kowite: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf(" Liczba a: %d \n Liczba b: %d \n Liczba c: %d \n", a, b, c);
+
+    //zadanie 1.2.7
+    int a;
+    printf("Podaj liczbe calkowita: \n");
+    scanf("%d", &a);
+    printf("Liczba calkowita + 1 to: %d \n", a+1);
+
+    //zadanie 1.2.8
+    int a,b,c;
+    printf("Podaj 3 liczby calkowite: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("Srednia arytmetyczna tych 3 cyfr to: %d", (a+b+c)/3);
+
+    //zadanie 1.2.9
+    float a;
+    printf("Podaj liczbe a: \n");
+    scanf("%f", &a);
+    printf("Pierwiastek z liczby a to: %f", sqrt(a));
+
+    //zadanie 1.2.10
+    float a;
+    printf("Podaj liczbe wymierna a: \n");
+    scanf("%f", &a);
+    printf("wartosc bezwzledna z liczby a to: %d", abs(a));
+
+    return 0;
+}
+*/

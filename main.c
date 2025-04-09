0754e4cd5ef3d92a1212ae8a556fb4374c12a42e
karol203 <karolnowakowski154@gmail.com>
2025-04-09 16:25:46 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+void wypisz(unsigned int n, int* tab){
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",tab[i]);
 
-main()
-{
-    printf("celciusze i farencjusze");
-    printf("Test Repo");
-    int fahr, celsius;
-    int lower, upper, step;
-    lower = 0;
-    upper = 300;
-    step = 20;
-    fahr = lower;
-    printf("Celsius: \t Fahrenheit:\n");
-    while(fahr <= upper){
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%7.1d %19.0d \n",celsius,fahr);
-        fahr = fahr + step;
     }
-    printf("Done");
+    printf("\n");
+}
+void zero(unsigned int n, int* tab){
+    int i;
+    for(i=0; i<n; i++)
+    {
+        tab[i]=0;
+    }
+    wypisz(n,tab);
+}
+void zad1b(unsigned int n, int* tab){
+    int i;
+    for(i=0; i<n; i++)
+    {
+        tab[i]=i;
+    }
+    wypisz(n,tab);
+}
+void zad1c(unsigned int n, int* tab){
+    int i;
+    for(i=0; i<n; i++)
+    {
+        tab[i] = tab[i] * 2;
+    }
+    wypisz(n,tab);
+}
+int main()
+{
+    /*
+    /// ZADANIE 4.2.0
+    int rozmiar=4;
+    int tab[rozmiar];
+    tab[0]=6; tab[1]=2; tab[2]=6; tab[3]=7;
+    wypisz(rozmiar, tab);
+    printf("%d\n",*tab);
+
+    /// ZADANIE 4.2.1A
+    zero(rozmiar, tab);
+
+    /// ZADANIE 4.2.1B
+    zad1b(rozmiar, tab);
+    */
+    /// ZADANIE 4.2.1C
+    int tab[]={4,5,13,51};
+    zad1c(4,tab);
+    return 0;
 }

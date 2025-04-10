24cf6e55e16cfe6aaee00fb197a3ee7bfb19b715
karol203 <karolnowakowski154@gmail.com>
2025-04-10 12:41:58 +0200

    Update Tablice.c

--- Tablice.c
+++ Tablice.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 void wypisz(unsigned int n, int* tab){
     int i;
     for(i=0;i<n;i++)
@@ -33,6 +34,85 @@ void zad1c(unsigned int n, int* tab){
     }
     wypisz(n,tab);
 }
+void zad1d(unsigned int n, int* tab){
+    int i;
+    for(i=0; i<n; i++)
+    {
+        if(tab[i]>0)
+        {
+            tab[i];
+        }
+        else
+        {
+            tab[i] = tab[i] * -1;
+        }
+    }
+    wypisz(n, tab);
+}
+float zad2a(unsigned int n,int* tab){
+    int i;
+    int suma = 0;
+    for(i=0; i<n; i++)
+    {
+        suma = suma + tab[i];
+    }
+    float srednia = suma / n;
+    return srednia;
+}
+int zad2b(unsigned int n, int* tab){
+    int i;
+    int suma = 0;
+    for(i=0; i<n; i++)
+    {
+        suma = suma + tab[i];
+    }
+    return suma;
+}
+int zad2c(unsigned int n,int* tab){
+    int i;
+    int suma = 0;
+    for(i=0; i<n; i++)
+    {
+        suma = suma + tab[i] * tab[i];
+    }
+    return suma;
+}
+int zad3(unsigned const int n,const int* tab){
+    int i;
+    int suma = 0;
+    for(i=0; i<n; i++)
+    {
+        suma = suma + tab[i];
+    }
+    int srednia = suma / n;
+    return srednia;
+}
+float zad4(unsigned int n, unsigned int* tab){
+    int i;
+    int iloczyn = 1;
+    for(i=0; i<n; i++)
+    {
+        iloczyn = iloczyn * tab[i];
+    }
+    return pow(iloczyn,1.0/n);
+}
+void zad6a(unsigned int n, int* tab1, int* tab2){
+    int i;
+    for(i=0; i<n; i++)
+    {
+        tab2[i] = tab1[i];
+    }
+    wypisz(n, tab2);
+}
+void zad6b(unsigned int n, int* tab1, int* tab2){
+    int i;
+    int j;
+    for(i=0; i<n; i++)
+    {
+        tab2[n-1-i]=tab1[i];
+    }
+    wypisz(n, tab2);
+}
 int main()
 {
     /*
@@ -48,9 +128,52 @@ int main()
 
     /// ZADANIE 4.2.1B
     zad1b(rozmiar, tab);
-    */
+
     /// ZADANIE 4.2.1C
     int tab[]={4,5,13,51};
     zad1c(4,tab);
+
+    /// ZADANIE 4.2.1D
+    int n = 4;
+    int tab[]={6,-8,16,-55};
+    zad1d(n, tab);
+
+    /// ZADANIE 4.2.2A
+    int n = 4;
+    int tab[]={5,5,4,4};
+    printf("%lf", zad2a(n, tab));
+
+    /// ZADANIE 4.2.2B
+    int n = 4;
+    int tab[] = {1,1,1,1};
+    printf("Suma elementow wynosi %d", zad2b(n, tab));
+
+    /// ZADANIE 4.2.2C
+    int n = 4;
+    int tab[] = {4,6,1,1};
+    printf("Suma kwadratow wynosi %d", zad2c(n, tab));
+
+    /// ZADANIE 4.2.3
+    int n = 4;
+    int tab[] = {4,4,5,5};
+    printf("Srednia arytmetyczna wynosi %d", zad3(n, tab));
+
+    /// ZADANIE 4.2.4
+    int n = 4;
+    int tab[] = {4,5,6,7};
+    printf("Srednia geometryczna wynosi %f", zad4(n, tab));
+
+    /// ZADANIE 4.2.6A
+    int n = 4;
+    int tab1[] = {3,4,5,6};
+    int tab2[] = {1,2,3,4};
+    zad6a(n, tab1, tab2);
+    */
+    /// ZADANIE 4.2.6B
+    int n = 4;
+    int tab1[] = {3,4,5,6};
+    int tab2[] = {1,2,3,4};
+    zad6b(n,tab1,tab2);
     return 0;
 }
+

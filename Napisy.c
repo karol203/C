3c0a387e5ab1d707d22626ae79891acb0781f715
karol203 <karolnowakowski154@gmail.com>
2025-04-17 12:53:31 +0200

    Update Napisy.c

--- Napisy.c
+++ Napisy.c
@@ -32,6 +32,40 @@ int porownaj(char* napis1, char* napis2)
         return 0;
     }
 }
+int pierwszy(char* napis1, char* napis2){
+    int i = 0;
+    while(napis1[i]!=0 || napis2[i]!=0)
+    {
+        if(napis1[i]<napis2[i])
+            return 1;
+        else if(napis1[i]<napis2[i])
+            return 0;
+        if(napis2[i]==0)
+        {
+            return 0;
+        }
+        i++;
+    }
+}
+void przepisz(char* napis1, char* napis2){
+    int i;
+    for(i=0;i<dlugosc(napis1);i++)
+    {
+        napis2[i]=napis1[i];
+    }
+    napis2[dlugosc(napis1)]=0;
+}
+void przepiszpierwszych(char* napis1, char* napisz2, unsigned int n){
+    int i;
+    for(i=0; i<dlugosc(napis1)&&i<n;i++)
+    {
+        napis2[i]=napis1[i];
+    }
+    napis2[i]=0;
+}
+void wypisz(char* napis){
+    printf("%s\n", napis);
+}
 int main()
 {
     /*
@@ -48,7 +82,15 @@ int main()
 
     /// ZADANIE 5.2.2
     printf("%d\n",dlugosc("Pracownia programowania to bardzo przyjemny przedmiot"));
-    */
+
     /// ZADANIE 5.2.3
     printf("%d\n",porownaj("Pierwszy napis", "Pierwszy napis"));
+    /// ZADANIE 5.2.4
+    printf("%d\n",pierwszy("Arbuz", "Banan"));
+    */
+    /// ZADANIE 5.2.5
+
+    /// ZADANIE 5.2.14
+    char* pierwszy="arbuz";
+    wypisz(pierwszy);
 }
